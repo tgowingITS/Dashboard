5383c9be62a3b795279c3e6b0c7eed8874f93e05
tgowingITS <tegan.gowing@inthestyle.com>
2022-02-23 16:10:39 +0000

    dynamic listing of data items

--- dashboard/src/Components/Sidebar/SidebarList.js
+++ dashboard/src/Components/Sidebar/SidebarList.js
@@ -4,17 +4,23 @@ const SidebarList = props => {
 
    if (props.items.length === 0) {
      return <h2 className = "sidebar-list__fallback">Data not loaded </h2>;
-   };
+   }
+   else {
 
    return <ul className = "sidebar-list">
      {props.items.map((data) => ( 
-       <DataItem 
+       <SidebarItem 
          key={data.id}
          title={data.title} 
        />
      ))}
 
    </ul>
-}
+
+  // return (
+  //   <div> HERE ARE YOUR DATA ITEMS</div>
+  // )
+   };
+  };
 
 export default SidebarList;

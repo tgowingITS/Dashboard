d8d1d5a194ce4d56b462b6175a9fa6f1c363f006
tgowingITS <tegan.gowing@inthestyle.com>
2022-02-23 16:08:55 +0000

    data placeholder props

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,11 +1,34 @@
 import './App.css';
-import Sidebar from ""
+import Sidebar from "./Components/Sidebar/Sidebar"
+
+const dummy_sidebar_data = [
+  {
+    id: 1,
+    title: 'Revenue'
+  },
+  {
+    id: 2,
+    title: 'Traffic'
+  },
+  {
+    id: 3,
+    title: 'Conversion'
+  },
+  {
+    id: 4,
+    title: 'AOV'
+  },
+  {
+    id: 5,
+    title: 'Returns'
+  },
+];
 
 const App = () => {
 
   return (
     <div>
-      <Sidebar />
+      <Sidebar items={dummy_sidebar_data} />
     </div>
   );
 };
